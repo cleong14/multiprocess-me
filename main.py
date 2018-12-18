5812370e0f413ea2173dfa165913f27547107b70
Chaz Leong <cleong14@gmail.com>
2018-12-17 19:09:39 -1000

    Still a WIP. Pushing up for in process code review.

--- main.py
+++ main.py
@@ -4,44 +4,52 @@ import requests
 from multiprocessing import Process, Queue, current_process
 
 menu_options = [
-    '1. Add a domain name\n'
-    '2. Start processing queue\n',
-    '3. Stop processing queue\n',
-    '4. Display logs\n',
-    '5. Exit'
+  '\n1. Add a domain name\n',
+  '2. Start processing queue\n',
+  '3. Stop processing queue\n',
+  '4. Display logs\n',
+  '5. Exit\n',
+  '\nPlease select a process from the menu'
 ]
 
 domain_list = [
-    'https://devleague.com',
-    'https://sudokrew.com',
-    'https://google.com',
-    'https://facebook.com'
+  'https://devleague.com',
+  'https://sudokrew.com',
+  'https://google.com',
+  'https://facebook.com'
 ]
 scraped_domain_list = []
+
+options_str = ''
+
+for options in menu_options:
+  options_str += options
 
 NUM_WORKERS = 4
 
 done_queue = Queue()  # This is messages from the child processes for parent
 process_queue = Queue()  # This is the domains to process
 
-
 def scraper(process_queue, done_queue):
   done_queue.put("{} starting".format(current_process().name))
+
   for domain in iter(process_queue.get, 'STOP'):
-    print('\nScraping Domain:', domain)
     result = requests.get(domain)
+    print("{}: Domain {} retrieved with {} bytes".format(current_process().name, domain, len(result.text)))
     done_queue.put("{}: Domain {} retrieved with {} bytes".format(current_process().name, domain, len(result.text)))
 
-def main(prompt):
-  options_str = '\n'
+  # Print initial menu / everytime the scraper is ran 
+  print(options_str)
 
-  for options in menu_options:
-    options_str += options
+for i in range(NUM_WORKERS):
+  Process(target=scraper, args=(process_queue, done_queue)).start()
+
+def main():
   print(options_str)
 
   while True:
     try:
-      value = int(input(prompt))
+      value = int(input())
 
       if value == 1:
         print('\nInput domain info below:\n')
@@ -51,17 +59,13 @@ def main(prompt):
         domain_list.append(domain_str)
         print('Domains: ', domain_list)
 
-        main('\nPlease select a process from the menu\n')
+        main()
 
       if value == 2:
-        print('\nStarting queue...\n')
+        print('\nProcessing...\n')
 
         for domain in domain_list:
           process_queue.put(domain)
-          print(domain, 'has been queued...')
-
-        for i in range(NUM_WORKERS):
-          Process(target=scraper, args=(process_queue, done_queue)).start()
           
         for message in iter(done_queue.get, 'STOP'):
           print(message)
@@ -71,7 +75,7 @@ def main(prompt):
       if value == 3:
         print('Stopping queue...')
 
-        return value
+        main()
 
       if value == 4:
         print('Logs below...')
@@ -96,4 +100,4 @@ def main(prompt):
 
 
 if __name__ == "__main__":
-  main('\nPlease select a process from the menu\n')
+  main()
